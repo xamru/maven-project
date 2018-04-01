593b54c4b4652647e2c74e9c778e76f7534315b6
xa <xa@derp.com>
2018-03-31 20:26:17 -0400

    corrected check

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param someone the name of a perosn
+   * @return greeting string
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This is soooo **
+ */
+package com.example;
